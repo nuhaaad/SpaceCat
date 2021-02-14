c534ace8a9ca05b9fb25e0a27996b2287fc1b498
nuhaaad <60096197+nuhaaad@users.noreply.github.com>
2021-02-14 21:28:17 +0100

    new

--- spacecat/script.js
+++ spacecat/script.js
@@ -1,38 +1,16 @@
-var meteor = document.getElementById("meteor");
-var meteortwo = document.getElementById("meteortwo");
-var character = document.getElementById("character");
-var jumping = 0;
+const character = document.getElementById("character";)
 
-meteortwo.addEventlistener('animationiteration', () => {
-          var random = Math.random()*3;
-          var top = (random*100)+150;
-          meteortwo.style.top = -(top) + "px";
-});
-setInterval(function(){
-  var characterTop =
-  parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-  if (jumping==0) {
-      character.style.top = (characterTop+3)+"px";
+// this function will make our character jump
+function jump(){
+if (character.classList != "jump") // with this it will not try to reset the animation over and over 
+    character.classList.add("jump");
+
+setTimeout(function() {
+   character.classList.remove("jump");
+  }, 300); // this animation lasts for 300ms
+ }
 }
-},10);
 
-function jump () {
-  jumping = 1;
-  let jumpCount = 0;
-  var jumpInterval = setInterval(function){
-    var characterTop =
-    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    if((characterTop>6)&&(counter<15)) {
-      character.style.top = (characterTop-5)+"px";
-    }
-
-    if(jumpCount>20){
-      clearInterval(jumpInterval);
-      jumping=0;
-      jumpCount=0;
-    }
-
-      jumpCount++;
-  },10);
-
-}
+document.addEventlistener("keydown", function (event) {
+  jump();
+});  // this is for any keyboard - (it jumps only once )
